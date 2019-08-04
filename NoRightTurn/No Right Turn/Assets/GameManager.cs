3bdf77fbd29c0591b3fe7be8a5a08c81ceb9159a
PhillyStein <phillxstein@gmail.com>
2019-08-04 02:11:30 +0100

    Core gameplay created
    Created base mechanic. Just need to add some levels!

---
+++ NoRightTurn/No Right Turn/Assets/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int currentLevel;
+
+    public GameObject[] levels;
+
+    public Movement player;
+
+    public GameObject winScreen,
+                        countdownPanel,
+                        finishLine;
+
+    public TextMeshProUGUI winText,
+                            pressText,
+                            countdownText;
+
+    public Vector2[] levelStartPos = { new Vector2(-8, -3.5f) },
+                        finishLinePos = { new Vector2(5.5f, 5.5f)};
+
+    public float[] levelStartRotation = { 0 };
+
+    public static GameManager instance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        StartLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(winScreen.activeInHierarchy)
+        {
+            if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+            {
+                winScreen.SetActive(false);
+                StartLevel();
+            }
+        }
+
+    }
+
+    public void StartLevel()
+    {
+        player.transform.position = levelStartPos[currentLevel];
+        player.rotation = levelStartRotation[currentLevel];
+        finishLine.transform.position = finishLinePos[currentLevel];
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        countdownPanel.SetActive(true);
+        countdownText.text = "3";
+        yield return new WaitForSeconds(1);
+        countdownText.text = "2";
+        yield return new WaitForSeconds(1);
+        countdownText.text = "1";
+        yield return new WaitForSeconds(1);
+        countdownText.text = "Go!";
+        yield return new WaitForSeconds(1);
+        countdownPanel.SetActive(false);
+        player.ClearStates();
+    }
+
+    public void WinLevel()
+    {
+        winText.text = "Level " + (currentLevel + 1) + " Complete!";
+        pressText.text = "Press < to go to the next level.";
+        winScreen.SetActive(true);
+    }
+
+    public void LoseLevel()
+    {
+        winText.text = "Too bad! Try again!";
+        pressText.text = "Press < to restart level.";
+        winScreen.SetActive(true);
+    }
+
+    public void NextLevel()
+    {
+        currentLevel++;
+
+        foreach(GameObject level in levels)
+        {
+            level.SetActive(false);
+        }
+
+        if (currentLevel <= levels.Length)
+        {
+            levels[currentLevel].SetActive(true);
+        }
+    }
+}
---
+++ NoRightTurn/No Right Turn/Assets/Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float rotation, 
+                    speed;
+
+    public Vector2 targetPos;
+
+    public bool goingToTurn,
+                isTurning,
+                canTurn,
+                winState,
+                loseState;
+
+    public Rigidbody2D carRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = transform.rotation.eulerAngles.z;
+        loseState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.eulerAngles = Vector3.forward * rotation;
+
+        if (!isTurning && !winState && !loseState)
+        {
+            Drive();
+        }
+
+        if(canTurn)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            {
+                goingToTurn = true;
+                canTurn = false;
+            }
+        }
+    }
+
+    public void Drive()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+    public void Turn()
+    {
+        if (rotation == 90)
+        {
+            rotation = 180;
+        }
+        else if (rotation == 180)
+        {
+            rotation = 270;
+        }
+        else if (rotation == 270)
+        {
+            rotation = 0;
+        }
+        else
+        {
+            rotation = 90;
+        }
+        goingToTurn = false;
+        isTurning = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "TurnTrigger")
+        {
+            if (goingToTurn)
+            {
+                isTurning = true;
+                Turn();
+            }
+        }
+
+        if(other.tag == "BeforeTurn")
+        {
+            canTurn = true;
+        }
+
+        if(other.tag == "FinishLine")
+        {
+            winState = true;
+            GameManager.instance.WinLevel();
+        }
+
+        if(other.tag == "Boundary")
+        {
+            loseState = true;
+            GameManager.instance.LoseLevel();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "BeforeTurn")
+        {
+            canTurn = false;
+        }
+    }
+
+    public void ClearStates()
+    {
+        winState = false;
+        loseState = false;
+    }
+}
